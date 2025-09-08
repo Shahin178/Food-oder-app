847936efc31cb62bca5d833587a772de2f8fdcd7
Shahin Bano <banoshahin89@gmail.com>
2025-09-09 02:55:09 +0530

    cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import ItemList from './itemList'
-import { clearCart } from '../utils/cartSlice';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./itemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
-  const dispatch= useDispatch();
-  const cartItems= useSelector((store)=>store.cart.items)
+  const dispatch = useDispatch();
+  const cartItems = useSelector((store) => store.cart.items);
   console.log(cartItems);
 
-  const handleClearCart=()=>{
-    dispatch(clearCart())
-  }
-  
-  return (
-    <div className='text-center m-4 p-4'>
-    {cartItems.length===0 && <h1 className='text-2xl font-bold p-2'>Your cart is empty!</h1>}
-    <h1 className='text-2xl font-bold p-2'>Cart</h1>
-    <div className='w-6/12 m-auto border border-gray-200 rounded-lg shadow'>
-    <button className='p-2 m-2 bg-emerald-600 rounded-lg text-white' onClick={handleClearCart}>Clear Cart</button>
-    <ItemList items={cartItems}/>
-    </div>
-      
-    </div>
-  )
-}
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
 
-export default Cart
+  return (
+    <div className="text-center m-4 p-4">
+      {cartItems.length === 0 ? (
+        <h1 className="text-2xl font-bold p-2">Your cart is empty!</h1>
+      ) : (
+        <>
+          <h1 className="text-2xl font-bold p-2">Cart</h1>
+          <div className="w-6/12 m-auto border border-gray-200 rounded-lg shadow p-4">
+            {/* Button aligned to the right */}
+            <div className="flex justify-end">
+              <button
+                className="px-4 mx-6 py-2 bg-red-500 hover:bg-red-600 rounded-lg text-white shadow-md transition duration-200"
+                onClick={handleClearCart}
+              >
+                Clear Cart
+              </button>
+            </div>
+
+            {/* Item list */}
+            <ItemList items={cartItems} />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
